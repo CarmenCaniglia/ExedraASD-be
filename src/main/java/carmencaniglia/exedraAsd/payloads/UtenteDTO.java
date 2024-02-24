8fdf12327f657484cacae1e8f3f660b8c84337e0
Carmen Caniglia <carmencaniglia@gmail.com>
2024-02-24 16:17:27 +0100

    fix utenteDTO

--- src/main/java/carmencaniglia/exedraAsd/payloads/UtenteDTO.java
+++ src/main/java/carmencaniglia/exedraAsd/payloads/UtenteDTO.java
@@ -10,7 +10,7 @@ public record UtenteDTO(@NotEmpty(message = "Il nome è un campo obbligatorio!")
                         String nome,
                         @NotEmpty(message = "Il cognome è un campo obbligatorio!")
                         String cognome,
-                        @NotEmpty(message = "La password è un campo obbligatorio!")
+                        //password messa obbligatoria nella registrazione nel Front-End
                         String password,
                         @NotEmpty(message = "L'email è un campo obbligatorio!")
                         @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "L'email inserita non è valida")
